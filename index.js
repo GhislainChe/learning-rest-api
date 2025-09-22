88ea5ef0ffb263e3def04dfc23baa25d96b2a070
GhislainChe <ghislainche2007@gmail.com>
2025-09-22 02:57:17 +0100

    arranged the users route

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import userRoutes from './routes/users.js'
+
 const app = express()
 const PORT = 5000
 
 app.use(bodyParser.json())
 
+app.use('/users', userRoutes)
+
 app.get('/', (req, res) => {
-    console.log('Working')
-    
     res.send('hello from ghislain')
 })
 
